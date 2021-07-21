dc3c61f823d8051ccff559916b28aa8d2eeb2f70
KazukiAsano <kentag2s2010@gmail.com>
2021-07-21 20:06:27 +0900

    activeadminによる管理者画面の導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@
 # お試し
 # Review.create!([{id:1,user_id:3,post_id:1,rate:2.0},{id:2,user_id:4,post_id:1,rate:4.5},{id:3,user_id:3,post_id:2,rate:2.0},{id:4,user_id:4,post_id:2,rate:2.5}])
 
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
